f286d42e1063e9e578ed8a1f2aab73a641f73ccf
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-06-15 11:48:27 +0530

    Create the Todo

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+#Todo-1 Create the movement of the cars
+#Todo-2 Create the player Movement
+#Todo-3 When player hits 280 ensure that it goes back to -280 and increase the speed of car
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
