b74f6721e67a2ae6542dcc4068da6889e87c9cb4
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-06-15 13:23:07 +0530

    Cars are moving

--- car_manager.py
+++ car_manager.py
@@ -6,23 +6,34 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        random_number=random.randint(0, len(COLORS)-1)
-        self.y_pos=random.randint(-275,275)
-        self.color(COLORS[random_number])
-        self.penup()
-        self.goto(x=281,y=self.y_pos)
-        self.move_speed=STARTING_MOVE_DISTANCE
+        self.all_cars=[]
+        self.create_car()
 
-    def car_move(self):
-        self.goto(x=(self.xcor()+self.move_speed),y=self.y_pos)
+
+    def create_car(self):
+        new_car=Turtle()
+        new_car.shape("square")
+        random_number = random.randint(0, len(COLORS) - 1)
+        new_car.y_pos = random.randint(-275, 275)
+        new_car.color(COLORS[random_number])
+        new_car.penup()
+        new_car.goto(x=281, y=new_car.y_pos)
+        new_car.move_speed = STARTING_MOVE_DISTANCE
+        self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.goto((car.xcor()-car.move_speed),car.ycor())
+
+        self.all_cars = [car for car in self.all_cars if car.xcor() > -320]
 
     def level_up(self):
-        self.move_speed+=MOVE_INCREMENT
+        for car in self.all_cars:
+            car.move_speed+=MOVE_INCREMENT
 
     def reset_position(self):
-        self.goto(310,(self.y_pos+random.randint(0,10)))
-        self.level_up()
+        for car in self.all_cars:
+            self.goto(310,(car.y_pos+random.randint(0,10)))
+            self.level_up()
--- main.py
+++ main.py
@@ -3,25 +3,29 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 #Todo-1 Create the movement of the cars
 #Todo-2 Create the player Movement
-#Todo-3 When player hits 280 ensure that it goes back to -280 and increase the speed of car
+#Todo-3 When player hits 280 ensure that it goes back to -280 and increase the speed of car_manager
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-# player=Player()
+player=Player()
 
 # Listen for key presses
 screen.listen()
-# screen.onkeypress(player.player_move(), "Up")
+screen.onkeypress(player.player_move(), "Up")
+car_manager = CarManager()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    car = CarManager()
-    car.car_move()
+    car_manager.move_cars()
+    if random.randint(1, 6) == 1:  # ~16% chance each loop
+        car_manager.create_car()
+
