d5a98c47e89d10635f588763b2b59c16bdd8e69b
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-06-15 14:06:34 +0530

    Scoreboard also done Now to look into the game over sequenc

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ import random
 #Todo-1 Create the movement of the cars
 #Todo-2 Create the player Movement
 #Todo-3 When player hits 280 ensure that it goes back to -280 and increase the speed of car_manager
+#Todo-4 Need To Make ScoreBoard
+#Todo-5 Game Over Screen
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+score = Scoreboard()
 
 player=Player()
 
@@ -29,6 +33,9 @@ while game_is_on:
     if random.randint(1, 6) == 1 :  # ~16% chance each loop
         car_manager.create_car()
 
-    if player.ycor()>=280:
+    if player.ycor()>=270:
         player.reset_position()
         car_manager.level_up()
+        score.update()
+
+    # if player.distance()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,23 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+FONT = ("Courier", 18, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.current_score=0
+        # self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.sety(270)
+        self.write(f"Level: {self.current_score}",False,align='center',font=FONT)
+
+    def update(self):
+        self.clear()
+        self.current_score+=1
+        self.write(f"Level: {self.current_score}",False,align='center',font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align='center',font=FONT)
