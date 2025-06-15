29288190f1acaafc53eba139572b8740d8b76d0e
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-06-15 12:26:29 +0530

    Figuring out where I am going wrong

--- car_manager.py
+++ car_manager.py
@@ -11,18 +11,18 @@ class CarManager(Turtle):
         super().__init__()
         self.shape("square")
         random_number=random.randint(0, len(COLORS)-1)
+        self.y_pos=random.randint(-275,275)
         self.color(COLORS[random_number])
         self.penup()
-        self.y_move=random.randint(-270,280)
-        self.x_move=STARTING_MOVE_DISTANCE
-        self.move_speed=10
+        self.goto(x=281,y=self.y_pos)
+        self.move_speed=STARTING_MOVE_DISTANCE
 
     def car_move(self):
-        self.goto(x=(self.xcor()+self.x_move),y=self.y_move)
+        self.goto(x=(self.xcor()+self.move_speed),y=self.y_pos)
 
     def level_up(self):
-        self.x_move+=MOVE_INCREMENT
+        self.move_speed+=MOVE_INCREMENT
 
     def reset_position(self):
-        self.goto(310,self.y_move)
+        self.goto(310,(self.y_pos+random.randint(0,10)))
         self.level_up()
